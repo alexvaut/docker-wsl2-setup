3740f4475eacbccc0fff412bae2bdf18a00ed254
Alexandre Vautier <alex@vautier.biz>
2021-12-16 11:23:04 +0100

    fix uninstall path

--- start-install-docker.ps1
+++ start-install-docker.ps1
@@ -7,7 +7,7 @@ Expand-Archive -Force __install\install.zip .\__install
 .\__install\install\install.ps1
 
 Move-Item -Force .\__install\install\start-dockerd.ps1 .\start-dockerd.ps1
-Move-Item -Force  .\__install\install\uninstall-docker.ps1 .\uninstall.ps1
+Move-Item -Force  .\__install\install\uninstall.ps1 .\uninstall.ps1
 
 Start-Sleep -Seconds 1
 
